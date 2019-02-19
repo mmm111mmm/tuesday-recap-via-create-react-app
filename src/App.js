d8b198ab752d2ee1b7fc88b757624bb431e3d918
newfivefour <newfivefour@newfivefour.com>
2019-02-19 18:43:54 +0100

    Here's the Tuesday recap but via create-react-app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class Dog extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hungry: "yes",
+        }
+        this.feed = this.feed.bind(this)
+    }
+    feed() {
+        this.setState({
+            hungry: "no"
+        })
+    }
+    render() {
+        return (
+            <div>Woof, woof: 
+                {this.props.name} 
+                <br />
+                Colour: {this.props.colour}
+                <br />
+                Hungry: {this.state.hungry}
+                <br />
+                  <button onClick={this.feed}>Feed</button>
+                <hr />
+            </div>                
+        );
+    }
+}
+
+class Playtime extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            treats: []
+        }
+    }
+    giveTreat = (treat) => {
+        this.state.treats.push(treat)
+        this.setState({
+            treats: this.state.treats
+        })
+    }
+    render() {
+        var treats = this.state.treats.map(function(item, i) {
+            return <li key={item+i}>{item}</li>
+        })
+        return (
+            <div>
+                <ul>
+                    {treats}
+                </ul>
+                <button onClick={() => this.giveTreat("stroke")}>
+                    stroke lovingly
+                </button>
+                <button onClick={() => this.giveTreat("ball")}>
+                    play ball
+                </button>
+                <button onClick={() => this.giveTreat("orange")}>
+                    give orange
+                </button>
+                { this.state.treats[this.state.treats.length-1] === "orange" 
+                  ?  <h1>ORANGES!!!!</h1>
+                  : <div></div>
+                }
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h3>Dogs</h3>
+          <Dog name="Kira" colour="brown and white"/>
+          <Dog name="Tess" colour="grey" />
+          <h3>Play with dogs!</h3>
+          <Playtime />
       </div>
     );
   }
